3886754f16b1dd834d05f8e5f3cd74b38482585c
Kamil Molwani <kmolwani93@gmail.com>
2018-05-28 01:14:34 -0400

    Added card HTML using JS; Updated moves counter

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
 /*
  * Create a list that holds all of your cards
  */
+ var cardList = ['fa-diamond', 'fa-paper-plane-o',
+                 'fa-anchor', 'fa-bolt',
+                 'fa-cube', 'fa-anchor',
+                 'fa-leaf', 'fa-bicycle',
+                 'fa-diamond', 'fa-bomb',
+                 'fa-leaf', 'fa-bomb',
+                 'fa-bolt', 'fa-bicycle',
+                 'fa-paper-plane-o', 'fa-cube']
 
+ /*
+  * Display the cards on the page
+  */
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+function generateCard(cardList) {
+  return `<li class='card'><i class='fa ${cardList}'></i></li>`;
+}
+
+function cardsStack() {
+  var unorderedListElement = document.querySelector('.deck');
+
+  var listCode = cardList.map(function(card) {
+    return generateCard(card);
+  });
+
+  unorderedListElement.innerHTML = listCode.join('');
+
+}
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,64 +45,75 @@ function shuffle(array) {
     return array;
 }
 
-var cards = document.querySelectorAll('.card');
-var cardOpen = [];
-var matchingCards = 0;
-
-cards.forEach( function(card) {
-  card.addEventListener('click', function(event) {
-      cardOpen.push(card.firstElementChild.className);
-
-        if (cardOpen.length <= 2) {
-          card.classList.add('open','show');
-          console.log(card.className);
-
-            if (cardOpen.length == 2 && cardOpen[0] === cardOpen[1]) { //checking if the cards match
-
-              var match = document.querySelectorAll('.open' , '.show') //finding all cards with class open & show
-
-              match.forEach ( function(matches) {
-                setTimeout(function() {
-                  matches.classList.remove('open', 'show'); //removing class open & show from the open cards
-                  matches.classList.add('match'); //adding class match to the open cards
-                }, 300);
-              });
-
-              cardOpen = []; // setting cards open counter back to zero
-
-              // checking for total matched cards
-              matchingCards += match.length;
-
-              if (matchingCards == 16 ){
-                alert ('Congratulations!')
-              }
-
-              } else if (cardOpen.length == 2 && cardOpen[0] != cardOpen[1]) { //checking if the cards do not match
-
-                    var flip = document.querySelectorAll('.open', '.show') //finding all cards with class open & show
-
-                    flip.forEach ( function(flips) {
-                      setTimeout(function() {
-                        flips.classList.remove('open','show') //removing class open & show from open cards
-                      }, 300);
-                    });
-
-                    cardOpen = []; // setting cards open counter back to zero
-
-              }
-      }
-  });
-});
-
-
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * set up the event listener for a card.
  */
+
+function matchingGame() {
+
+  var cards = document.querySelectorAll('.card');
+  var cardOpen = [];
+  var matchingCards = 0;
+  var moves = 0;
+  // console.log(cards);
+
+  cards.forEach( function(card) {
+    card.addEventListener('click', function(event) {
+        cardOpen.push(card.firstElementChild.className);
+        moves += 1;
+        var movesUpdate = document.querySelector('.moves');
+        movesUpdate.textContent = moves;
+        // console.log(moves);
+
+        if (moves == 20) {
+          var star = document.querySelector('.fa-star');
+          star.classList.remove('fa-star');
+        } else if (moves == 50) {
+          var star = document.querySelector('.fa-star');
+          star.classList.remove('fa-star');
+        }
+
+          if (cardOpen.length <= 2) {
+            card.classList.add('open','show');
+
+              if (cardOpen.length == 2 && cardOpen[0] === cardOpen[1]) { //checking if the cards match
+
+                var match = document.querySelectorAll('.open' , '.show') //finding all cards with class open & show
+
+                match.forEach ( function(matches) {
+                  setTimeout(function() {
+                    matches.classList.remove('open', 'show'); //removing class open & show from the open cards
+                    matches.classList.add('match'); //adding class match to the open cards
+                  }, 0);
+                });
+
+                cardOpen = []; // setting cards open counter back to zero
+
+                // checking for total matched cards
+                matchingCards += match.length;
+
+                if (matchingCards == 16 ){
+                  alert ('Congratulations!')
+                }
+
+                } else if (cardOpen.length == 2 && cardOpen[0] != cardOpen[1]) { //checking if the cards do not match
+
+                      var flip = document.querySelectorAll('.open', '.show') //finding all cards with class open & show
+
+                      flip.forEach ( function(flips) {
+                        setTimeout(function() {
+                          flips.classList.remove('open','show') //removing class open & show from open cards
+                        }, 300);
+                      });
+
+                      cardOpen = []; // setting cards open counter back to zero
+                }
+        }
+    });
+  });
+};
+
+shuffle(cardList);
+cardsStack();
+matchingGame()
